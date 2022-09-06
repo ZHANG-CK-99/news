b248661b21f703347c5479f026f2f8c5cfdd989c
ZHANG-CK <1693002443@qq.com>
2022-09-06 18:16:29 +0800

    完成了登录页面布局

---
+++ src/views/login/index.vue
@@ -0,0 +1,86 @@
+<template>
+  <div>
+    <!-- 头部导航 -->
+    <van-nav-bar title="登录" class="nav-bar" />
+    <!-- 表单 -->
+    <van-form @submit="onSubmit">
+      <van-field
+        v-model="mobile"
+        name="mobile"
+        placeholder="请输入手机号"
+        :rules="[
+          { required: true, message: '请输入手机号' },
+          {
+            pattern:
+              /^(?:(?:\+|00)86)?1(?:(?:3[\d])|(?:4[5-79])|(?:5[0-35-9])|(?:6[5-7])|(?:7[0-8])|(?:8[\d])|(?:9[189]))\d{8}$/,
+            message: '手机号格式错误'
+          }
+        ]"
+      >
+        <template #label>
+          <span class="toutiao toutiao-shouji"></span>
+        </template>
+      </van-field>
+      <van-field
+        v-model="code"
+        name="code"
+        placeholder="请输入密码"
+        :rules="[
+          { required: true, message: '请输入密码' },
+          { pattern: /[0-9]{6}/, message: '验证码格式错误' }
+        ]"
+      >
+        <template #label>
+          <span class="toutiao toutiao-yanzhengma"></span>
+        </template>
+      </van-field>
+      <div style="margin: 16px">
+        <van-button round block type="info" native-type="submit"
+          >提交</van-button
+        >
+      </div>
+    </van-form>
+  </div>
+</template>
+<script>
+export default {
+  name: 'LoginPage',
+  props: {},
+  components: {},
+  data() {
+    return {
+      mobile: '',
+      code: ''
+    }
+  },
+  computed: {},
+  // 生命周期 - 创建完成(访问当前this实例)
+  created() {},
+  // 生命周期 - 挂载完成(访问DOM元素)
+  mounted() {},
+  methods: {
+    onSubmit(values) {
+      console.log('submit', values)
+    }
+  }
+}
+</script>
+<style scoped lang="less">
+.nav-bar {
+  background-color: #3296fa;
+  :deep(.van-nav-bar__title) {
+    color: #fff;
+  }
+}
+:deep(.van-form) {
+  .van-cell__title {
+    flex: 1;
+  }
+  .van-cell__value {
+    flex: 20;
+  }
+  .toutiao {
+    font-size: 40px;
+  }
+}
+</style>
