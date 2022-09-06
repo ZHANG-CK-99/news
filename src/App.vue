ea004428ada8a61ce2bab01c2d98788281dac373
ZHANG-CK <1693002443@qq.com>
2022-09-06 11:51:37 +0800

    feat:初始化了news

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,20 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
-  </div>
+  <div>45645496</div>
 </template>
-
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
+<script>
+export default {
+  name: 'App',
+  props: {},
+  components: {},
+  data() {
+    return {}
+  },
+  computed: {},
+  // 生命周期 - 创建完成(访问当前this实例)
+  created() {},
+  // 生命周期 - 挂载完成(访问DOM元素)
+  mounted() {},
+  methods: {}
 }
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+</script>
+<style scoped lang="less"></style>
